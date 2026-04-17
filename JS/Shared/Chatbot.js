70960c39463c8d03265d90b9957c2abda407679e
Error404 <ashur.nichols@gmail.com>
2026-04-17 12:15:42 -0500

    updated link

--- JS/Shared/Chatbot.js
+++ JS/Shared/Chatbot.js
@@ -54,7 +54,7 @@ async function fetchGameRecommendation() {
     };
 
     // UPDATE THIS WITH YOUR ACTIVE CODESPACE URL
-    const backendURL = "https://zany-xylophone-v6gx5xw9gj6gfx545-8000.app.github.dev/api/recommend";
+    const backendURL = "https://ubiquitous-space-acorn-69rjpjwqr6wgf5775-8000.app.github.dev//api/recommend";
 
     try {
         const response = await fetch(backendURL, {
