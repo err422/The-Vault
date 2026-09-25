94ff70a05c8eb26d926325d2046de1421145ac4b
Error404 <ashur.nichols@gmail.com>
2026-09-25 17:24:21 -0500

    Remove navigation debug logging

--- JS/Pages/Home.js
+++ JS/Pages/Home.js
@@ -14,11 +14,3 @@ function updateClock() {
 
 updateClock();
 setInterval(updateClock, 1000);
-
-// Add click handlers for nav items (excluding fullscreen which is handled above)
-document.querySelectorAll('.nav-item:not(#fullscreen-btn)').forEach(item => {
-    item.addEventListener('click', function () {
-        const label = this.querySelector('.nav-label').textContent;
-        console.log(label + ' clicked');
-    });
-});
